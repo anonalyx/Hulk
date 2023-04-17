2b6305fd00d379881f17021d3e19af5d23cb5353
adamjmcwilliams <52553806+adamjmcwilliams@users.noreply.github.com>
2023-04-17 11:02:16 -0700

    Load Navbar Dynamically (#8)

--- src/app/static/js/main.js
+++ src/app/static/js/main.js
@@ -1,10 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('navbar.html')
+      .then(response => response.text())
+      .then(data => {
+        document.querySelector('#navbar-container').innerHTML = data;
+        addNavbarEventListeners();
+      })
+      .catch(error => {
+        console.error('Error loading navbar:', error);
+      });
+});
+
+function addNavbarEventListeners() {
     const brand_image = document.getElementById('brand-image');
     const brand = document.getElementById('brand')
     const nav_search_button = document.getElementById('nav_search_button')
     const nav_calcalc_button = document.getElementById('nav_calcalc_button')
-    const nav_profile_button= document.getElementById('nav_profile_button')
-
+    const nav_profile_button = document.getElementById('nav_profile_button')
 
     brand.addEventListener('click', () => {
         console.log('redirecting home')
@@ -26,4 +37,4 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('redirecting home')
         window.location.href = '/profile';
     });
-});
+}
